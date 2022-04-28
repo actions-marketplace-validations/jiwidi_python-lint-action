423439d6b44d12ad73075bf6245bd6078bbe8d17
Jiwidi <fhjaime96@gmail.com>
2022-04-28 14:44:00 +0200

    some minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,27 +12,29 @@ set -e
 
 echo "===== Running Python Linter - '$2' mode ====="
 
-echo "Running black"
 if [ "$2" = "high" ]; then
+    echo "Running autoflake"
+    autoflake "$1" ${4} -c
+    echo "Running black"
     black "$1" ${5} --check
     echo "Running isort"
     isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running autoflake"
-    autoflake "$1" ${4} -c
 
 elif [ "$2" = "medium" ]; then
+    echo "Running autoflake"
+    autoflake "$1" ${4} -c
+    echo "Running black"
     black "$1" ${5} --check
     echo "Running isort"
     isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running autoflake"
-    autoflake "$1" ${4} -c
 
 else
-    black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} --diff
     echo "Running autoflake"
     autoflake "$1" ${4} -c
+    echo "Running black"
+    black "$1" ${5} --check
+    echo "Running isort"
+    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
 fi
 
 echo "Done 🎉" ; echo ""
