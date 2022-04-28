1468bbcdbced0ab4006ce57aef9d37da9177da30
Jiwidi <fhjaime96@gmail.com>
2022-04-28 14:50:24 +0200

    some minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,37 +4,36 @@ set -e
 
 # source = $1
 # strict = $2
-# mypy-options = $3
-# autoflake-options = $4
+# autoflake-options = $3
+# isort-options = $4
 # black-options = $5
-# isort-options = $6
 
 
 echo "===== Running Python Linter - '$2' mode ====="
 
 if [ "$2" = "high" ]; then
     echo "Running autoflake"
-    autoflake "$1" ${4} -c
+    autoflake "$1" ${3} -c
+    echo "Running isort"
+    isort "$1"/**/*.py -m 3 --trailing-comma ${4} -c
     echo "Running black"
     black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
 
 elif [ "$2" = "medium" ]; then
     echo "Running autoflake"
-    autoflake "$1" ${4} -c
+    autoflake "$1" ${3} -c
+    echo "Running isort"
+    isort "$1"/**/*.py -m 3 --trailing-comma ${4} -c
     echo "Running black"
     black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
 
 else
     echo "Running autoflake"
-    autoflake "$1" ${4} -c
+    autoflake "$1" ${3} -c
+    echo "Running isort"
+    isort "$1"/**/*.py -m 3 --trailing-comma ${4} -c
     echo "Running black"
     black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
 fi
 
 echo "Done 🎉" ; echo ""
